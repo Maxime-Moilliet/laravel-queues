1aad7d03de65d395d41bff92f3ef568a7cfb5f2c
Maxime Mllt <mlltmax66@gmail.com>
2024-02-14 21:04:00 +0100

    send register mail

--- app/Http/Controllers/Auth/RegisterUserController.php
+++ app/Http/Controllers/Auth/RegisterUserController.php
@@ -6,19 +6,23 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\RegisterUserRequest;
+use App\Mail\RegisterUserMail;
 use App\Models\User;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Mail;
 
 final class RegisterUserController extends Controller
 {
     public function __invoke(RegisterUserRequest $request): RedirectResponse
     {
-        User::create([
+        $user = User::create([
             'name' => $request->get('name'),
             'email' => $request->get('email'),
             'password' => Hash::make(strval($request->get('password'))),
         ]);
+
+        Mail::to($user)->send(new RegisterUserMail($user));
 
         return redirect()->back();
     }
